149c664fd2088360b695cc7a9e35af7da5ae50fb
jmoore6364 <jmoore6364@yahoo.com>
2019-03-27 14:49:02 -0700

    Basics ported in from raw js

--- src/app/scroll-bar/scroll-bar.component.ts
+++ src/app/scroll-bar/scroll-bar.component.ts
@@ -1,45 +1,94 @@
-import { 
-  Component, 
-  OnInit,
-  ViewChild,
-  ElementRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'app-scroll-bar',
+  selector: 'essex-scroll-bar',
   templateUrl: './scroll-bar.component.html',
-  styleUrls: ['./scroll-bar.component.css']
+  styleUrls: ['./scroll-bar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ScrollBarComponent implements OnInit {
-  @ViewChild('scrollbar') scrollbarElementRef: ElementRef;
-  @ViewChild('slider') sliderElementRef: ElementRef;
-  @ViewChild('frame') frameElementRef: ElementRef;
+  @ViewChild('container') container: ElementRef<HTMLElement>;
+  @ViewChild('slider') slider: ElementRef<HTMLElement>;
+  @ViewChild('scrollbar') scrollbar: ElementRef<HTMLElement>;
+  @ViewChild('frame') frame: ElementRef<HTMLElement>;
 
-  scrollbarElement: Object = this.scrollbarElementRef.nativeElement;
-  sliderElement: Object = this.sliderElementRef.nativeElement;
-  frameElement: Object = this.frameElementRef.nativeElement;
-  dragging: Boolean = false;
-  dragStartedX: Number = 0;
-  dragOffsetX: Number = 0;
-  lastPos: Number = 0;
-  sliderSize: Number = this.frame.nativeElement.scrollWidth / 5;
-  scrollAmount: Number = this.frame.scrollWidth / (scrollbar.clientWidth - sliderSize);
+  sliding: boolean = false;
+  moving: string = 'none';
+  slideStartedX: number = 0;
+  slideOffsetX: number = 0;
+  lastPos: number = 0;
+  sliderSize: number = 0;
+  scrollAmount: number = 0; 
 
   constructor() { }
 
-  mouseDown(event) {
-    console.log(this.slider);
-    console.log(event);
+  mouseDown(event: MouseEvent) {
+    this.sliderStart(event);
   }
 
-  mouseUp(event) {
-
+  mouseUp(event: MouseEvent) {
+    this.sliderStop();
   }
 
-  mouseMove(event) {
-   //console.log(event);
+  mouseMove(event: MouseEvent) {
+    if (!this.sliding)
+      return;
+    if (this.lastPos === 0)
+      this.lastPos = event.clientX;
+    if (this.lastPos === event.clientX) {
+      this.moving = "none";
+    } else if (this.lastPos < event.clientX) {
+      if (this.moving === "left")
+        this.sliderStart(event);
+      this.scrollLeft(event.clientX);
+      this.moving = "right";
+    } else {
+      if (this.moving === "right")
+        this.sliderStart(event);
+      this.scrollRight(event.clientX);
+      this.moving = "left";
+    }
+    this.lastPos = event.clientX;
   }
+
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.sliderSize = this.scrollbar.nativeElement.clientWidth / 5;
+    this.scrollAmount = this.frame.nativeElement.scrollWidth / (this.scrollbar.nativeElement.clientWidth - this.sliderSize);
+    this.slider.nativeElement.style.width = this.sliderSize + 'px';
+  }
+
+  private sliderStart(event: MouseEvent) {
+    this.moving = "none";
+    this.sliding = true;
+    this.slideStartedX = event.clientX;
+    this.slideOffsetX = this.slider.nativeElement.offsetLeft;
+  }
+
+  private sliderStop() {
+    this.sliding = false;
+  }
+
+  private scrollLeft(mouseX) {
+    var margin = (mouseX - this.slideStartedX) + this.slideOffsetX;
+    if ((margin + this.sliderSize) > this.scrollbar.nativeElement.clientWidth) {
+      margin = margin - ((margin + this.sliderSize) - this.scrollbar.nativeElement.clientWidth);
+    } else {
+      this.frame.nativeElement.scrollLeft = this.scrollAmount * margin;
+    }
+    this.slider.nativeElement.style.marginLeft = margin + 'px';
+  }
+
+  scrollRight(mouseX) {
+    var margin = (mouseX - this.slideStartedX) + this.slideOffsetX;
+    if (margin < 0)
+      return;
+    this.frame.nativeElement.scrollLeft = this.scrollAmount * margin;
+    this.slider.nativeElement.style.marginLeft = margin + 'px';
+  }
+
 }
+
